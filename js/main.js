121b5b72181bd59793f8cff3a2dddb66d81c0956
Tucker Whitehouse <TuckerWhitehouse@gmail.com>
2013-11-16 19:04:22 -0500

    Init

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-$('[data-timeago]').each(function(){
-    var $t = $(this);
-    $t.text( moment( $t.text() ).fromNow() );
+var $header     = $('#header')
+  , $header_navbar = $header.find('.navbar')
+
+  , $content    = $('#content')
+
+  , $colophon   = $('#colophon')
+
+  , $footer     = $('#footer');
+
+
+
+
+$(window).on('load hashchange', function (e) {
+    var hash        = location.hash.split('/')
+      , category    = hash[1]
+      , page        = hash[2];
+
+    $.ajax({
+        url: 'pages/'+ (hash.length == 3 ? hash.slice(1,3).join('/') : 'index') +'.html'
+    }).done(function (html, status, xhr) {
+        $content.html(html);
+
+        $header_navbar.find('.nav > li').removeClass('active');
+        hash[1] && $header_navbar.find('.nav > li[data-category="'+ hash[1] +'"]').addClass('active');
+    });
 });
